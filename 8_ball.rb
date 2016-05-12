f11c11c6d4ab24a31c71cbde1df5be3e8093f47c
Rachel Prows <rachel.prows@icloud.com>
2016-05-12 16:30:41 -0600

    basic Magic 8 Ball

---
+++ 8_ball.rb
@@ -0,0 +1,42 @@
+# get user input question
+# Allow quit
+# Print goodbye message
+#quit
+
+def greeting
+	puts "\nWelcome to the Ruby Magic Eight Ball. Please ask a yes or no question. Or type quit to exit."
+	ask
+end
+
+def ask
+	user_input = gets.strip.downcase
+	if user_input == 'quit'
+		puts "Thanks for asking the Magic Eight Ball. Please play again!"
+		exit (0)
+	else 
+		question(user_input)
+	end
+end
+
+def question(user_input)
+	puts "\nThe Magic Eight Ball says:\n\n"
+	if user_input.include?('?')
+		puts response
+	elsif user_input == 'quit'
+		puts "Thanks for playing."
+		exit (0)
+	else
+		puts "I'm sorry. I didn't catch that. Please ask again!"
+		ask
+	end
+end
+
+def response
+	answer = [ "It is certain", "Without a doubt", "Yes, definitely!", "You can count on it!", "Don't count on it", "My sources say no", "Not a snowball's chance in hell." ]
+ 	answer.sample
+end
+
+
+while true
+greeting 
+end
