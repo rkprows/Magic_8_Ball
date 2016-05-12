3db83a2a2b62016233dda0e0ee34071a0604922e
Rachel Prows <rachel.prows@icloud.com>
2016-05-12 17:44:22 -0600

    most bonus minus duplicates

--- 8_ball.rb
+++ 8_ball.rb
@@ -1,7 +1,7 @@
-# get user input question
-# Allow quit
-# Print goodbye message
-#quit
+
+def array
+	@answer = [ "It is certain", "Without a doubt", "Yes, definitely!", "You can count on it!", "Don't count on it", "My sources say no", "Not a snowball's chance in hell." ]
+end
 
 def greeting
 	puts "\nWelcome to the Ruby Magic Eight Ball. Please ask a yes or no question. Or type quit to exit."
@@ -21,22 +21,28 @@ end
 def question(user_input)
 	puts "\nThe Magic Eight Ball says:\n\n"
 	if user_input.include?('?')
-		puts response
+		puts @answer.sample
 	elsif user_input == 'quit'
 		puts "Thanks for playing."
 		exit (0)
+	elsif user_input == 'add answers'
+		puts "What answer would you like to add?"
+		user_input = gets.strip
+		@answer << user_input
+		puts "#{user_input} has been successfully added to response pool."
+	elsif user_input == 'print answers'
+		puts "All possible responses are: #{@answer}"
+	elsif user_input == 'reset answers'
+		array
+		puts "Answers have been reset."
 	else
 		puts "I'm sorry. I didn't catch that. Please ask again!"
 		ask
 	end
 end
 
-def response
-	answer = [ "It is certain", "Without a doubt", "Yes, definitely!", "You can count on it!", "Don't count on it", "My sources say no", "Not a snowball's chance in hell." ]
- 	answer.sample
-end
 
-
+array
 while true
-greeting 
+	greeting 
 end
